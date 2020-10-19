e6147afcce41d6e8ce9eafb00bc21ab5cd3600a8
timodokter <timo.dokter@student.rocvf.nl>
2020-10-19 11:19:50 +0100

    opdracht 1 is af

---
+++ opdracht1/opdracht 1.2/javascript.js
@@ -0,0 +1,3 @@
+for(i = 0; i < 10; i++) {
+    console.log("de waarde van i is " + i);
+}
---
+++ opdracht1/opdracht 1.3/javascript.js
@@ -0,0 +1,7 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "mondeo",
+    aantalwielen: 4,
+    kleur: "blauw",
+}
+console.log(mijnauto.kleur);
