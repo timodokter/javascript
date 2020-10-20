3fcf37552166b9886ff701b0da6c08e492ba149d
timodokter <timo.dokter@student.rocvf.nl>
2020-10-20 09:29:51 +0100

    opdracht 2.1 is af

---
+++ DOM/vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Dit is de paginatitel"
+document.getElementById("subkop").innerText = "Dit is de subkop"
+document.getElementById("aap1").src = "img/little-monkey.jpg"
+document.getElementById("aap2").src = "img/monkey.jpg"
