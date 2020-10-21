83272217d16d130c9a486b8403bba4eeaa04ed46
timodokter <timo.dokter@student.rocvf.nl>
2020-10-21 11:23:30 +0100

    hij werkt

--- arrays/script.js
+++ arrays/script.js
@@ -1,3 +1,16 @@
-var plaatjes = document.getElementsByTagName("img")
-var random = Math.floor((Math.random() * 9) + 1)
-plaatjes[5].src = "img/"
+var plaatjes = document.getElementsByTagName("img");
+var random = 0;
+
+var randomnumbers = [];
+
+while(randomnumbers.length < 9) {
+    random = Math.floor((Math.random() * 9) + 1);
+    if (randomnumbers.lastIndexOf(random) == -1) {
+        randomnumbers.push(random);
+    }
+}
+random = 0;
+for (var plaatje in plaatjes) {
+    plaatjes[plaatje].src = "img/game" + randomnumbers[random] + ".jpg"
+    random++
+    }
