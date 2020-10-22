c047a27ce33c7314389376f39cb2dd3f370e99db
timodokter <timo.dokter@student.rocvf.nl>
2020-10-22 10:57:14 +0100

    4.3 is af

--- functies/4.3/script.js
+++ functies/4.3/script.js
@@ -1,9 +1,9 @@
 var arrayogen = [1,2,3,4,5];
 var arrayneus = [1,2,3,4,5];
 var arraymond = [1,2,3,4,5];
-var tellerogen = 0;
-var tellerneus = 0;
-var tellermond = 0;
+var tellerogen = 1;
+var tellerneus = 1;
+var tellermond = 1;
 var slideholderogen = document.getElementById("ogen")
 var slideholderneus = document.getElementById("neus")
 var slideholdermond = document.getElementById("mond")
@@ -11,3 +11,46 @@ var slideholdermond = document.getElementById("mond")
 slideholderogen.style.backgroundImage = "url('img/ogen/crookOgen1.jpg')";
 slideholderneus.style.backgroundImage = "url('img/neuzen/crookNeus1.jpg')";
 slideholdermond.style.backgroundImage = "url('img/monden/crookMond1.jpg')";
+
+slideholderogen.addEventListener("click", function () {
+    slideholderogen.style.backgroundImage = "url('img/ogen/crookOgen" + getogen() + ".jpg')";
+});
+slideholderneus.addEventListener("click", function () {
+    slideholderneus.style.backgroundImage = "url('img/neuzen/crookNeus" + getneuzen() + ".jpg')";
+});
+slideholdermond.addEventListener("click", function () {
+    slideholdermond.style.backgroundImage = "url('img/monden/crookMond" + getmonden() + ".jpg')";
+});
+
+function getogen() {
+    if (tellerogen >= arrayogen.length) {
+        tellerogen = 1;
+    } else {
+        tellerogen++;
+    }
+    console.log(tellerogen)
+    return tellerogen;
+}
+
+function getneuzen() {
+    if (tellerneus>= arrayneus.length) {
+        tellerneus = 1;
+    } else {
+        tellerneus++;
+    }
+    console.log(tellerneus)
+    return tellerneus;
+}
+
+
+function getmonden() {
+    if (tellermond >= arraymond.length) {
+        tellermond = 1;
+    } else {
+        tellermond++;
+    }
+    console.log(tellermond)
+    return tellermond;
+}
+
+
