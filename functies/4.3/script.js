f803b58c9b70c1b2f0a103b664ff81da7b94ecbd
timodokter <timo.dokter@student.rocvf.nl>
2020-10-22 10:27:00 +0100

    de placeholder voor de plaatjes staan er

---
+++ functies/4.3/script.js
@@ -0,0 +1,13 @@
+var arrayogen = [1,2,3,4,5];
+var arrayneus = [1,2,3,4,5];
+var arraymond = [1,2,3,4,5];
+var tellerogen = 0;
+var tellerneus = 0;
+var tellermond = 0;
+var slideholderogen = document.getElementById("ogen")
+var slideholderneus = document.getElementById("neus")
+var slideholdermond = document.getElementById("mond")
+
+slideholderogen.style.backgroundImage = "url('img/ogen/crookOgen1.jpg')";
+slideholderneus.style.backgroundImage = "url('img/neuzen/crookNeus1.jpg')";
+slideholdermond.style.backgroundImage = "url('img/monden/crookMond1.jpg')";
