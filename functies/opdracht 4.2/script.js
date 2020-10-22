de6e95306510d066d937a07219009b2d1738e6b5
timodokter <timo.dokter@student.rocvf.nl>
2020-10-22 10:00:00 +0100

    4.2 is af

--- functies/opdracht 4.2/script.js
+++ functies/opdracht 4.2/script.js
@@ -1,3 +1,18 @@
 var plaatjes = [1,2,3,4,5,6,7,8,9];
+var teller = 0;
 var slideholder = document.getElementById("slideholder");
 slideholder.style.backgroundImage = "url('img/game1.jpg')";
+
+slideholder.addEventListener("click", function () {
+    slideholder.style.backgroundImage = "url('img/game" + getGame() + ".jpg')";
+});
+
+function getGame() {
+    if (teller >= plaatjes.length) {
+        teller = 1;
+    } else {
+        teller++;
+    }
+    console.log(teller)
+    return teller;
+}
