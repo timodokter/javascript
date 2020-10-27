08db812507a49decd139075ffa694cb99af160e2
timodokter <timo.dokter@student.rocvf.nl>
2020-10-27 12:54:23 +0100

    de tekst staat nu allemaal nu nog het punten systeem

--- memorygame/script.js
+++ memorygame/script.js
@@ -1,2 +1,11 @@
-var beurt = "blablabla";
+//de variabelen
+var speler1 = "Timo";
+var speler2 = "Dylan";
+var beurt = " heeft de beurt";
+var puntenspeler1 = 0;
+var puntenspeler2 = 0;
+
+//de tekst boven in de top-bar
 document.getElementById("tekstbeurt").innerHTML = beurt;
+document.getElementById("tekstspeler1").innerHTML = speler1 + " : " + puntenspeler1;
+document.getElementById("tekstspeler2").innerHTML = speler2 + " : " + puntenspeler2;
