01985354d93b5301c6edf61173df4765dfb57749
timodokter <timo.dokter@student.rocvf.nl>
2020-10-29 11:48:57 +0100

    de plaatjes staan er nu de overlay

--- memorygame/script.js
+++ memorygame/script.js
@@ -1,14 +1,14 @@
 //de aangeroepde functions
 
+
 //de variabelen
 var speler1 = "Timo";
 var speler2 = "Dylan";
 var beurt = " heeft de beurt";
 var puntenspeler1 = 0;
 var puntenspeler2 = 0;
-var pics;
 var aantal = 0;
-var i = 1
+var tester = 0;
 
 Boolean; beurtspeler1 = true;
 Boolean; zet = true;
@@ -20,6 +20,31 @@ document.getElementById("tekstspeler2").innerHTML = speler2 + " : " + puntenspel
 
 function dooverlay() {
     overlay = document.getElementsByClassName("overlay")
+}
+
+//picture-holder lader
+function pictureholderlader() {
+    pictureHolder = document.createElement("div")
+    pictureHolder.className = "picture-holder"
+}
+
+//fotos lader
+function fotolader() {
+    var container = document.getElementById("container");
+    var images = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg"];
+    for (aantal = 0; aantal < 2; aantal++) {
+        for (var i = 0; i < 9; i++) {
+            // tester + 1;
+            // console.log(tester)
+            var fmg = document.createElement("img");
+            fmg.setAttribute("src", "img/" + images[i]);
+            fmg.setAttribute("class", "foto");
+            fmg.setAttribute("alt", images[i]);
+            console.log(i);
+            console.log("img/" + images[i]);
+            container.appendChild(fmg);
+        }
+    }
 }
 
 //punten-, beurtsysteem
